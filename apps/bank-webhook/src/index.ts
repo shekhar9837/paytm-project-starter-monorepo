b24c689c613a8a109b0571d08ed8269e40ea5887
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-07-25 23:01:41 +0530

    .

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,49 @@
+import express from "express"
+import db from "@repo/db/client"
+
+const app = express();
+app.use(express.json())
+
+app.post("", async (req, res)=>{
+
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+
+    try {
+        
+    await db.balance.update({
+        where:{
+            userId: paymentInformation.userId
+        },
+        data : {
+            amount: {
+                increment: paymentInformation.amount
+            }
+        }
+    })
+    await db.onRampTransaction.update({
+        where:{
+            token: paymentInformation.token
+        },
+        data : {
+            status:"Success",
+        }
+    })
+    res.status(200).json({
+        message:"Captured"
+    })
+    }
+    catch(e){
+        console.error(e),
+        res.status(411).json({
+            message:"Error while processing webhook"
+        })
+    }
+
+
+})
+
+app.listen(3003)
